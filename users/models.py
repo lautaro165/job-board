a8ea02ba6665a9dd0ff0c24825aee28222e9a73f
lautaro165 <lautaro25@outlook.com.ar>
2025-09-16 17:09:59 -0300

    Updated the user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = (
+        ('dev', 'Developer'),
+        ('company', 'Company'),
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='dev')
