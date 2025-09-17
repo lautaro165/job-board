ed4f24145fa98ca9cfe2589ff00cbf18cf97b9ba
lautaro165 <lautaro25@outlook.com.ar>
2025-09-16 22:11:39 -0300

    Created a Company model

--- companies/models.py
+++ companies/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from users.models import CustomUser
 
 # Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, null=True)
+    website = models.URLField(blank=True, null=True, unique=True)
+    logo = models.ImageField(upload_to="company_logos/", blank=True, null=True)
+    
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="created_companies")
+
+    followers = models.ManyToManyField(CustomUser,related_name="followed_companies",blank=True)
+
+    def __str__(self):
+        return str(self.name)
