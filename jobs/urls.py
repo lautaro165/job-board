9f03f517d37552440a8d2d96b7a3e794ea7a3112
lautaro165 <lautaro25@outlook.com.ar>
2025-09-17 19:42:16 -0300

    Created post_job view

--- jobs/urls.py
+++ jobs/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("get-jobs-list/", views.get_jobs_list, name="get_jobs_list")
+    path("get-jobs-list/", views.get_jobs_list, name="get_jobs_list"),
+    path("post-job/", views.post_job, name="post_job")
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -1,4 +1,5 @@
 from rest_framework.decorators import permission_classes, api_view
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -13,3 +14,15 @@ def get_jobs_list(request):
     serializer = JobPostSerializer(jobs, many=True)
 
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+@permission_classes([IsAuthenticated])
+def post_job(request):
+    data = request.data
+
+    serializer = JobPostSerializer(data=data, context={"user":request.user})
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
