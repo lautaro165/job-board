473dd6a6f065b71887198183b4c491e11a7d8e62
lautaro165 <lautaro25@outlook.com.ar>
2025-09-22 17:58:32 -0300

    Fixed an error in the conftest.py fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,51 @@
+import pytest
+
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from users.models import CustomUser
+from companies.models import Company
+from jobs.models import JobPost
+from applications.models import Application
+
+@pytest.fixture
+def user():
+    return CustomUser.objects.create_user(username="random_user",email="random_user@gmail.com",password="123456", role="employee")
+
+@pytest.fixture
+def company(user):
+    return Company.objects.create(created_by=user, name="random_company")
+
+@pytest.fixture
+def job(user,company):
+    return JobPost.objects.create(owner=user,title="random_job",description="", company=company,min_wage=2000,max_wage=3000)
+
+@pytest.fixture
+def user_2():
+    return CustomUser.objects.create_user(username="random_user_2",email="random_user_2@gmail.com",password="123456", role="employee")
+
+
+@pytest.fixture
+def application(user, job):
+    return Application.objects.create(applicant=user.employee_profile, job=job)
+
+@pytest.fixture
+def user_tokens(user):
+    refresh = RefreshToken.for_user(user)
+    access = refresh.access_token
+    return {
+        "refresh": str(refresh),
+        "access": str(access),
+        "refresh_token_expires_time": int(refresh.lifetime.total_seconds()),
+        "access_token_expires_time": int(access.lifetime.total_seconds())
+    }
+
+@pytest.fixture
+def user_2_tokens(user_2):
+    refresh = RefreshToken.for_user(user_2)
+    access = refresh.access_token
+    return {
+        "refresh": str(refresh),
+        "access": str(access),
+        "refresh_token_expires_time": int(refresh.lifetime.total_seconds()),
+        "access_token_expires_time": int(access.lifetime.total_seconds())
+    }
