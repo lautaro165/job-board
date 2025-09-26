33d4e1ff567ea056cd8e03e7ac9da9ecc30281ce
lautaro165 <lautaro25@outlook.com.ar>
2025-09-26 18:12:29 -0300

    Created an apply_to_job view

--- applications/urls.py
+++ applications/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import *
+from . import views
 
 urlpatterns = [
-    
+    path("apply-to-job/<int:job_id>/", views.apply_to_job, name="apply_to_job")
 ]
--- applications/views.py
+++ applications/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.decorators import permission_classes, api_view
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+
+from jobs.models import JobPost
+from .serializers import ApplicationSerializer
 
 # Create your views here.
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def apply_to_job(request, job_id):
+    try:
+        job = JobPost.objects.get(id=job_id)
+    except JobPost.DoesNotExist:
+        return Response({"error": f"No job found with id {job_id}"}, status=status.HTTP_404_NOT_FOUND)
+    
+    data = request.data.copy()
+    data["applicant"] = request.user.id
+    data["job"] = job.id
+    
+    serializer = ApplicationSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
