428e578488fcdd0dec44c8e6300c76807fc61f25
lautaro165 <lautaro25@outlook.com.ar>
2025-09-26 18:34:21 -0300

    Modified the create method for the ApplicationSerializer

--- applications/serializers.py
+++ applications/serializers.py
@@ -7,3 +7,9 @@ class ApplicationSerializer(ModelSerializer):
         model = Application
         fields = "__all__"
         read_only_fields = ["status", "applicant", "job", "created_at", "updated_at"]
+
+    def create(self, validated_data):
+        user = self.context["user"]
+        job = self.context["job"]
+
+        return Application.objects.create(applicant=user, job=job, **validated_data)
