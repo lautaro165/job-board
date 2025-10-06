fa3f3bfdd84476277ff5e39b130bf024e0b02148
lautaro165 <lautaro25@outlook.com.ar>
2025-10-06 07:12:08 -0300

    Created a register_user view and implemented the CustomUser serializers

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import *
+from . import views
 
 urlpatterns = [
-    
+    path("register_user/", views.register_user, name="register_user")
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from jobs.models import JobPost
+# from .models import Application
+from .serializers import CustomUserSerializer, CustomUserRegistrationSerializer
 
 # Create your views here.
+
+@api_view(["POST"])
+def register_user(request):
+    serializer = CustomUserRegistrationSerializer(data=request.data)
+
+    if serializer.is_valid():
+        user = serializer.save()
+        refresh = RefreshToken.for_user(user)
+
+        return Response({
+            "message": "User registered successfully",
+            "user": CustomUserSerializer(user).data,
+            "tokens": {
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+            }
+        }, status=status.HTTP_201_CREATED)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
