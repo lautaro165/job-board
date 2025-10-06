8720ca800e842783fb9f81476be591ca08847729
lautaro165 <lautaro25@outlook.com.ar>
2025-10-06 07:10:24 -0300

    Implemented two serializers for user registering and displaying (CustomUser)

---
+++ users/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework.serializers import ModelSerializer, CharField
+
+from .models import CustomUser
+
+class CustomUserRegistrationSerializer(ModelSerializer):
+    password = CharField(write_only=True, min_length=6)
+    
+    class Meta:
+        model=CustomUser
+        fields=["id", "username", "email", "first_name", "last_name", "joined_at","role", "password"]
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data.get("email"),
+            password=validated_data["password"]
+        )
+        return user
+    
+    
+class CustomUserSerializer(ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ["id", "username", "email", "first_name", "last_name","joined_at", "role"]
+        read_only_fields = ["id", "email", "joined_at"]
