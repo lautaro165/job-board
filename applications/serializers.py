1255f906b3154fc3513439fa7916142467c99f8e
lautaro165 <lautaro25@outlook.com.ar>
2025-12-06 17:50:39 -0300

    Removed unnecessary create method

--- applications/serializers.py
+++ applications/serializers.py
@@ -7,12 +7,6 @@ class ApplicationSerializer(ModelSerializer):
         model = Application
         fields = "__all__"
         read_only_fields = ["status", "applicant", "job", "created_at", "updated_at"]
-
-    def create(self, validated_data):
-        user = self.context["user"]
-        job = self.context["job"]
-
-        return Application.objects.create(applicant=user, job=job, **validated_data)
     
 class ApplicationResponseSerializer(ModelSerializer):
     class Meta:
