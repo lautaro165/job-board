b24ef81d81f325ed24a0cc5478e41ca6fd12cb3c
lautaro <lautaro25@outlook.com.ar>
2025-12-11 23:23:42 -0300

    Created ApplicationStatusUpdate serializer for valitation

--- jobs/serializers.py
+++ jobs/serializers.py
@@ -1,7 +1,12 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, ChoiceField, CharField, Serializer
+
 from .models import JobPost
 
 class JobPostSerializer(ModelSerializer):
     class Meta:
         model = JobPost
         fields = ["id", "title", "description", "owner", "company", "min_wage", "max_wage"]
+        
+class ApplicationStatusUpdateSerializer(Serializer):
+    status = ChoiceField(choices=["accepted", "rejected", "reviewed"])
+    message = CharField()
