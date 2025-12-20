45586fe682c304adc4062ae0ef39e919979b77d8
lautaro165 <lautaro25@outlook.com.ar>
2025-12-20 16:39:19 -0300

    Created a new respond_to_application_service

--- applications/services.py
+++ applications/services.py
@@ -1,11 +1,30 @@
-from applications.models import Application
+from applications.exceptions import ForbiddenApplicationStatusUpdate
+from applications.models import Application, ApplicationResponse
 from rest_framework.exceptions import ValidationError
 
-def apply_to_job(user, job):
+def apply_to_job_service(user, job):
     if Application.objects.filter(applicant=user).exists():
         raise ValidationError("You already applied this job")
 
     return Application.objects.create(
         applicant=user,
         job=job,
+    )
+
+def respond_to_application_service(
+    application,
+    responder,
+    status,
+    message=None
+):
+    if application.job.owner != responder:
+        raise ForbiddenApplicationStatusUpdate()
+
+    application.status = status
+    application.save()
+
+    return ApplicationResponse.objects.create(
+        application=application,
+        responder=responder,
+        message=message,
     )
