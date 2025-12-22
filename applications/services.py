ec70ce0076f8e3b244c1596533614561a9739088
lautaro165 <lautaro25@outlook.com.ar>
2025-12-22 14:05:28 -0300

    Modified the withdraw_application_service

--- applications/services.py
+++ applications/services.py
@@ -1,8 +1,11 @@
 from applications.exceptions import ForbiddenApplicationStatusUpdate
 from applications.models import Application, ApplicationResponse
+
 from rest_framework.exceptions import ValidationError, PermissionDenied
 
-VALID_STATUSES = ["accepted", "rejected", "reviewed"]
+from debug import print_debug_message
+
+VALID_RESPONSE_STATUSES = ["accepted", "rejected", "reviewed"]
 
 def apply_to_job_service(user, job):
     if Application.objects.filter(applicant=user).exists():
@@ -22,7 +25,7 @@ def respond_to_application_service(
     if application.job.owner != responder:
         raise ForbiddenApplicationStatusUpdate()
 
-    if status not in VALID_STATUSES:
+    if status not in VALID_RESPONSE_STATUSES:
         raise ValueError("Invalid status")
 
     application.status = status
@@ -34,7 +37,11 @@ def respond_to_application_service(
         message=message,
     )
 
-def withdraw_application_service(user, instance):
-    if instance.applicant != user:
+def withdraw_application_service(user, application):
+    if application.applicant != user:
         raise PermissionDenied("You cannot withdraw an application that is not yours")
-    instance.delete()
+
+    application.status = "withdrawn"
+    application.save()
+
+    return application
