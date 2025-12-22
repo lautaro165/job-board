1fcf08d0f1faaaec332ab07f1af6d0a076d6d3f6
lautaro165 <lautaro25@outlook.com.ar>
2025-12-22 14:10:48 -0300

    Fixed a syntax error

--- applications/urls.py
+++ applications/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("apply-to-job/<int:job_id>/", views.ApplyToJobView.as_view(), name="apply_to_job"),
-    path("responde-to-application/<int:application_id>/", views.RespondToApplicationView.as_view(), name="responde_to_application"),
+    path("respond-to-application/<int:application_id>/", views.RespondToApplicationView.as_view(), name="respond_to_application"),
     path("withdraw-application/<int:application_id>/", views.WithdrawApplicationView.as_view(), name="withdraw_application"),
     path("get-user-applications/", views.UserApplicationsListView.as_view(), name="get_user_applications"),
     path("get-job-applications/<int:job_id>/", views.JobApplicationsListView.as_view(), name="get_job_applications")
