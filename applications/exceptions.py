3fe06542b4e6de4882fe1d4ee7e68bdf047a5a35
lautaro165 <lautaro25@outlook.com.ar>
2025-12-27 17:11:02 -0300

    Created two new Custom exceptions for application views

--- applications/exceptions.py
+++ applications/exceptions.py
@@ -2,4 +2,13 @@ from rest_framework import status
 from rest_framework.exceptions import APIException
 
 class ForbiddenApplicationStatusUpdate(APIException):
-    pass
+    status_code = status.HTTP_403_FORBIDDEN
+    default_detail = 'You are not authorized to update this application'
+
+class ApplicationAlreadyExists(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = 'Application already exists'
+
+class TryingToApplyToOwnJob(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = 'Trying to apply to own job'
