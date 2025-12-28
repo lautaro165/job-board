e825abbfb1f90ab47add66c8f02c2fb9e4275753
lautaro165 <lautaro25@outlook.com.ar>
2025-12-28 17:59:59 -0300

    Created a custom filter for JobPost model

---
+++ jobs/filters.py
@@ -0,0 +1,11 @@
+import django_filters
+
+from .models import JobPost
+
+class JobPostFilter(django_filters.FilterSet):
+    min_wage = django_filters.NumberFilter(field_name='salary', lookup_expr='gte')
+    max_wage = django_filters.NumberFilter(field_name='salary', lookup_expr='lte')
+
+    class Meta:
+        model = JobPost
+        fields = ["title", "owner", "company", "employment_type"]
