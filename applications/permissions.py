b3118c2374ac543860905c92a2a565d67ed59cab
lautaro <lautaro25@outlook.com.ar>
2026-04-21 20:26:07 -0300

    Created a new IsApplicationOwnerOrJobOwner permission

--- applications/permissions.py
+++ applications/permissions.py
@@ -6,3 +6,10 @@ class IsJobOwner(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return obj.job.owner == request.user
+
+class IsApplicationOwnerOrJobOwner(BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        # obj es una Application
+        # Permitir si es el applicant o si es el job owner
+        return obj.applicant == request.user or obj.job.owner == request.user
