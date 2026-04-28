6923dcadee852c35ee9d3e13e8cbef29332725ed
lautaro <lautaro25@outlook.com.ar>
2026-04-28 20:20:18 -0300

    Added a create method for ApplicationSerializer

--- applications/serializers.py
+++ applications/serializers.py
@@ -1,13 +1,20 @@
 from rest_framework.serializers import ModelSerializer, Serializer, CharField, ChoiceField
+from rest_framework.serializers import ValidationError
 
 from .models import Application, ApplicationResponse, ApplicationStatus
-
+from .services import apply_to_job_service
 
 class ApplicationSerializer(ModelSerializer):
     class Meta:
         model = Application
         fields = "__all__"
         read_only_fields = ["status", "applicant", "job", "created_at", "updated_at"]
+        
+    def create(self, validated_data):
+        user = self.context["user"]
+        job = self.context["job"]
+        
+        return apply_to_job_service(user=user, job=job)
     
 class ApplicationResponseSerializer(ModelSerializer):
     class Meta:
