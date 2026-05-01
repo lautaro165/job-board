527928cad9e1862cc4473335cf888729d8fd194f
lautaro <lautaro25@outlook.com.ar>
2026-05-01 14:47:45 -0300

    Created a base CompanySerializer and two ones that inherit from it

--- companies/serializers.py
+++ companies/serializers.py
@@ -2,8 +2,8 @@ from rest_framework import serializers
 from .models import Company
 
 
-class PublicCompanySerializer(serializers.ModelSerializer):
-    followers_count = serializers.IntegerField(source="followers.count", read_only=True)
+class CompanySerializer(serializers.ModelSerializer):
+    followers_count = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Company
@@ -15,18 +15,10 @@ class PublicCompanySerializer(serializers.ModelSerializer):
             "logo",
             "followers_count",
         ]
+
+class PublicCompanySerializer(CompanySerializer):
+    pass
         
-class OwnerCompanySerializer(serializers.ModelSerializer):
-    followers_count = serializers.IntegerField(source="followers.count", read_only=True)
-
-    class Meta:
-        model = Company
-        fields = [
-            "id",
-            "name",
-            "description",
-            "website",
-            "logo",
-            "created_at",
-            "followers_count",
-        ]
+class OwnerCompanySerializer(CompanySerializer):
+    class Meta(CompanySerializer.Meta):
+        fields = CompanySerializer.Meta.fields + ["created_at"]
