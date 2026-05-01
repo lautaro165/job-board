5c0a04c5cd51cb3a827f304e6a465e416cbc2768
lautaro <lautaro25@outlook.com.ar>
2026-05-01 14:49:22 -0300

    Created a simple CompanyListCreateView with an overwritten get_queryset()

--- companies/views.py
+++ companies/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+
+from django.db.models import Count
+
+from .serializers import PublicCompanySerializer
+from .models import Company
 
 # Create your views here.
+
+class CompanyListCreateView(generics.ListCreateAPIView):
+    serializer_class = PublicCompanySerializer
+    
+    def get_queryset(self):
+        return Company.objects.annotate(
+            followers_count=Count("followers")
+        )
