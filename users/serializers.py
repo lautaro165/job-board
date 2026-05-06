490e03661213082da067d636ef1bcf58c55b2f4d
lautaro <lautaro25@outlook.com.ar>
2026-05-06 14:41:53 -0300

    Refactor LoginAPIView

--- users/serializers.py
+++ users/serializers.py
@@ -1,49 +1,82 @@
-from rest_framework.serializers import ModelSerializer, CharField
+from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
+
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from django.contrib.auth import authenticate
 
 from .models import CustomUser
 
-class CustomUserRegistrationSerializer(ModelSerializer):
-    PASSWORD_MIN_LENGTH = 6
+class LoginUserSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+    
+    def validate(self, attrs):
+        username = attrs.get('username')
+        password = attrs.get('password')
 
-    password = CharField(write_only=True, min_length=PASSWORD_MIN_LENGTH)
-    password2 = CharField(write_only=True, min_length=PASSWORD_MIN_LENGTH)
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if not user:
+                raise serializers.ValidationError('Invalid credentials.')
+            
+            if not user.is_active:
+                raise serializers.ValidationError('User is disabled.')
+        else:
+            raise serializers.ValidationError('Must include "username" and "password".')
 
-    class Meta:
-        model=CustomUser
-        fields=["id", "username", "email", "first_name", "last_name", "date_joined","role", "password", "password2"]
-
-    def validate_email(self, value):
+        refresh = RefreshToken.for_user(user)
         
-        if CustomUser.objects.filter(email=value).exists():
-            raise ValidationError("This email is already in use")
-        
-        return value
+        return {
+            'user': user,
+            'access': str(refresh.access_token),
+            'refresh': str(refresh),
+            'access_expires': int(refresh.access_token.lifetime.total_seconds()),
+            'refresh_expires': int(refresh.lifetime.total_seconds())
+        }
+    
 
-    def validate_password2(self, value):
-        
-        if not value.strip():
-            raise ValidationError("You must write your password twice.")
-        
-        return value
+# class CustomUserRegistrationSerializer(serializers.ModelSerializer):
+#     PASSWORD_MIN_LENGTH = 6
 
-    def validate(self, data): 
-        password = data.get("password")
-        password2 = data.get("password2")
+#     password = serializers.CharField(write_only=True, min_length=PASSWORD_MIN_LENGTH)
+#     password2 = serializers.CharField(write_only=True, min_length=PASSWORD_MIN_LENGTH)
 
-        if not password == password2:
-            raise ValidationError("Passwords don't match")
+#     class Meta:
+#         model=CustomUser
+#         fields=["id", "username", "email", "first_name", "last_name", "date_joined","role", "password", "password2"]
+
+#     def validate_email(self, value):
         
-        return data
+#         if CustomUser.objects.filter(email=value).exists():
+#             raise ValidationError("This email is already in use")
+        
+#         return value
 
-    def create(self, validated_data):
-        validated_data.pop("password")
-        validated_data.pop("password2")
-        user = CustomUser.objects.create_user(**validated_data)
-        return user
+#     def validate_password2(self, value):
+        
+#         if not value.strip():
+#             raise ValidationError("You must write your password twice.")
+        
+#         return value
+
+#     def validate(self, data): 
+#         password = data.get("password")
+#         password2 = data.get("password2")
+
+#         if not password == password2:
+#             raise ValidationError("Passwords don't match")
+        
+#         return data
+
+#     def create(self, validated_data):
+#         validated_data.pop("password")
+#         validated_data.pop("password2")
+#         user = CustomUser.objects.create_user(**validated_data)
+#         return user
     
     
-class CustomUserSerializer(ModelSerializer):
+class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ["id", "username", "email", "first_name", "last_name","date_joined", "role"]
--- users/views.py
+++ users/views.py
@@ -1,12 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics
+from rest_framework.permissions import AllowAny
+
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenRefreshView
 
 # from jobs.models import JobPost
 # from .models import Application
-from django.contrib.auth import authenticate
-from .serializers import CustomUserSerializer, CustomUserRegistrationSerializer
+from .serializers import LoginUserSerializer
+
 
 # Create your views here.
 
@@ -18,43 +21,12 @@ def get_user_tokens(user):
     return refresh_token, access_token, refresh_token_expires_time, access_token_expires_time
 
 class LoginAPIView(APIView):
+    permission_classes = [AllowAny]
+    serializer_class = LoginUserSerializer
+
     def post(self, request):
-        username = request.data.get("username")
-        password = request.data.get("password")
-
-        if not (username and password):
-            return Response({"error": "You must provide a username and a password"}, status=status.HTTP_400_BAD_REQUEST)
-
-        user = authenticate(request, username=username, password=password)
-
-        if user is None:
-            return Response({"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
-
-        refresh_token, access_token, refresh_token_expires_time, access_token_expires_time = get_user_tokens(user)
-
-        return Response({
-            "refresh": str(refresh_token),
-            "access": str(access_token),
-            "refresh_token_expires_time": refresh_token_expires_time,
-            "access_token_expires_time": access_token_expires_time
-        }, status=status.HTTP_200_OK)
-
-
-class RegisterUserView(APIView):
-    def post(self, request):
-        serializer = CustomUserRegistrationSerializer(data=request.data)
-
-        if serializer.is_valid():
-            user = serializer.save()
-            refresh = RefreshToken.for_user(user)
-
-            return Response({
-                "message": "User registered successfully",
-                "user": CustomUserSerializer(user).data,
-                "tokens": {
-                    "refresh": str(refresh),
-                    "access": str(refresh.access_token),
-                }
-            }, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer = LoginUserSerializer(data=request.data)
+        
+        serializer.is_valid(raise_exception=True)
+        
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
