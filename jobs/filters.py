dffd2121abaeab829ad41773eaca915b419fa256
lautaro <lautaro25@outlook.com.ar>
2026-05-10 13:33:35 -0300

    Modified JobPost FilterSet

--- jobs/filters.py
+++ jobs/filters.py
@@ -3,9 +3,13 @@ import django_filters
 from .models import JobPost
 
 class JobPostFilter(django_filters.FilterSet):
+    title = django_filters.CharFilter(field_name="title", lookup_expr="icontains")
+    location = django_filters.CharFilter(field_name="location", lookup_expr="icontains")
+    
     min_salary = django_filters.NumberFilter(field_name='salary', lookup_expr='gte')
     max_salary = django_filters.NumberFilter(field_name='salary', lookup_expr='lte')
 
+
     class Meta:
         model = JobPost
-        fields = ["title", "owner", "company", "employment_type"]
+        fields = ["company", "employment_type"]
--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,5 @@
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
-from rest_framework import generics, status
-from rest_framework.response import Response
+from rest_framework import generics
 
 from .filters import JobPostFilter
 from .models import JobPost
