42a10d73d70fcacd5dc749c6e71b8d95f854333d
lautaro <lautaro25@outlook.com.ar>
2026-05-10 13:40:45 -0300

    Added custom company filter in JobPostFilter

--- jobs/filters.py
+++ jobs/filters.py
@@ -5,6 +5,7 @@ from .models import JobPost
 class JobPostFilter(django_filters.FilterSet):
     title = django_filters.CharFilter(field_name="title", lookup_expr="icontains")
     location = django_filters.CharFilter(field_name="location", lookup_expr="icontains")
+    company = django_filters.CharFilter(field_name="company", lookup_expr="icontains")
     
     min_salary = django_filters.NumberFilter(field_name='salary', lookup_expr='gte')
     max_salary = django_filters.NumberFilter(field_name='salary', lookup_expr='lte')
@@ -12,4 +13,4 @@ class JobPostFilter(django_filters.FilterSet):
 
     class Meta:
         model = JobPost
-        fields = ["company", "employment_type"]
+        fields = ["employment_type"]
