8de24c6135e8276c96b5cab8566022314c62e03f
lautaro <lautaro25@outlook.com.ar>
2026-05-13 08:46:17 -0300

    Resolved circular import

--- jobs/querysets.py
+++ jobs/querysets.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-from .models import JobPost
-
 class JobPostQuerySet(models.QuerySet):
     def visible(self):
-        return self.exclude(status=JobPost.ARCHIVED)
+        return self.exclude(status="ARCHIVED")
