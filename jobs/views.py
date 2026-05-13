adf9f54d58e15e5906a4eb2a123ad4d3ed488908
lautaro <lautaro25@outlook.com.ar>
2026-05-13 13:22:24 -0300

    Fixed a prop error with JobStatus

--- jobs/views.py
+++ jobs/views.py
@@ -5,6 +5,7 @@ from .filters import JobPostFilter
 from .models import JobPost
 from .permissions import IsJobOwner
 from .serializers import JobPostSerializer
+from .choices import JobStatuses
 
 # Create your views here.
 
@@ -37,7 +38,7 @@ class JobPostRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
         return super().update(request, *args, **kwargs)
     
     def perform_destroy(self, instance):
-        instance.status = JobPost.ARCHIVED
+        instance.status = JobStatuses.ARCHIVED
         instance.save()
 
 class JobPostRetrieveView(generics.RetrieveAPIView):
