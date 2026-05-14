3b7695015e2925666d93740848501b672447f2c3
lautaro <lautaro25@outlook.com.ar>
2026-05-14 10:50:33 -0300

    Added explicit fields in company serializers

--- companies/serializers.py
+++ companies/serializers.py
@@ -5,16 +5,16 @@ from .models import Company
 class CompanySerializer(serializers.ModelSerializer):
     class Meta:
         model = Company
-        fields = "__all__"
+        fields = ["id", "name", "description", "website"]
 
 class PublicCompanySerializer(serializers.ModelSerializer):
     followers_count = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Company
-        fields = "__all__"
+        fields = ["id", "name", "description", "website", "followers_count"]
 
 class OwnerCompanySerializer(serializers.ModelSerializer):
     class Meta:
         model = Company
-        fields = "__all__"
+        fields = ["id", "name", "description", "website", "created_at", "updated_at"]
