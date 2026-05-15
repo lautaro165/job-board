09e4b9a81cb3d7822e155d598f59e2f4411a7a31
lautaro <lautaro25@outlook.com.ar>
2026-05-15 17:10:49 -0300

    Created test_serializer_missing_not_required_fields

--- tests/serializers/test_jobs_serializers.py
+++ tests/serializers/test_jobs_serializers.py
@@ -25,3 +25,22 @@ class TestJobPostSerializer:
         
         serializer = JobPostSerializer(data=data)
         assert not serializer.is_valid()
+        
+    @pytest.mark.parametrize(
+        'missing_field',
+        [
+            "company",
+            "salary",
+            "employment_type",
+            "status",
+        ]
+    )
+    @pytest.mark.django_db
+    def test_serializer_missing_not_required_fields(self, missing_field, valid_job_post_data):
+        
+        data = valid_job_post_data.copy()
+        
+        data.pop(missing_field)
+        
+        serializer = JobPostSerializer(data=data)
+        assert serializer.is_valid(), serializer.errors
