516f03b3497a84230ba1c24063879ca92cb7195e
lautaro <lautaro25@outlook.com.ar>
2026-05-15 16:54:14 -0300

    Created test_serializer_missing_required_fields

---
+++ tests/serializers/test_jobs_serializers.py
@@ -0,0 +1,27 @@
+import pytest
+from unittest.mock import patch, MagicMock
+from rest_framework.exceptions import ValidationError
+from rest_framework_simplejwt.tokens import TokenError
+
+from jobs.serializers import JobPostSerializer, JobPostListSerializer
+
+
+class TestJobPostSerializer:
+    
+    @pytest.mark.parametrize(
+        'missing_field',
+        [
+            'title',
+            'description',
+            'location',
+        ]
+    )
+    @pytest.mark.django_db
+    def test_serializer_missing_required_fields(self, missing_field, valid_job_post_data):
+        
+        data = valid_job_post_data.copy()
+        
+        data.pop(missing_field)
+        
+        serializer = JobPostSerializer(data=data)
+        assert not serializer.is_valid()
