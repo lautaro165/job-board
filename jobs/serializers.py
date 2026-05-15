689895eea3efd341bdbd2d2c950bbd00503ad057
lautaro <lautaro25@outlook.com.ar>
2026-05-15 20:23:46 -0300

    Split JobPostListCreateView to handle validation logic

--- jobs/serializers.py
+++ jobs/serializers.py
@@ -27,3 +27,33 @@ class JobPostListSerializer(ModelSerializer):
     class Meta:
         model = JobPost
         fields = ['id', 'title', 'company', 'location', 'status', 'employment_type', 'salary']
+        read_only_fields = fields
+        
+class JobPostCreateSerializer(ModelSerializer):
+
+    class Meta:
+        model = JobPost
+
+        fields = [
+            'title',
+            'description',
+            'company',
+            'location',
+            'status',
+            'employment_type',
+            'salary'
+        ]
+
+    def validate_company(self, value):
+
+        user = self.context['request'].user
+
+        is_owner = value.owner == user
+        is_admin = value.admins.filter(id=user.id).exists()
+
+        if not (is_owner or is_admin):
+            raise ValidationError(
+                "You cannot post new jobs in this company."
+            )
+
+        return value
