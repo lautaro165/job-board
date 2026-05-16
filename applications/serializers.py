a33b122ac435f0d53cb933a56c7bd9e5d4ae1e95
lautaro <lautaro25@outlook.com.ar>
2026-05-16 14:42:03 -0300

    Removed commented ApplicationSerializer

--- applications/serializers.py
+++ applications/serializers.py
@@ -3,18 +3,6 @@ from rest_framework import serializers
 from .models import Application, ApplicationResponse, ApplicationStatus
 from .services import apply_to_job_service
 
-# class ApplicationSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Application
-#         fields = ["id", "applicant", "job", "cover_letter", "resume", "status", "created_at", "updated_at"]
-#         read_only_fields = ["status", "applicant", "job", "created_at", "updated_at"]
-        
-#     def create(self, validated_data):
-#         user = self.context["user"]
-#         job = self.context["job"]
-        
-#         return apply_to_job_service(user=user, job=job)
-
 class _ApplicationBaseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Application
