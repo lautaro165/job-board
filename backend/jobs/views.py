5485b3480ec6ae63f7b6de94227b7dd20bd663ca
lautaro <lautaro25@outlook.com.ar>
2026-05-18 17:26:59 -0300

    Created a ResumeAnalysisView

--- backend/jobs/views.py
+++ backend/jobs/views.py
@@ -1,11 +1,21 @@
 from rest_framework.permissions import AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework import generics
 
-from .filters import JobPostFilter
-from .models import JobPost
-from .permissions import IsJobOwner
+from django.shortcuts import get_object_or_404
+
+from applications.serializers import ResumeAnalysisSerializer
+
+
 from .serializers import JobPostCreateSerializer, JobPostSerializer, JobPostListSerializer
+from .utils.pdf_handler import extract_text_from_pdf
+from .utils.info import get_job_post_info
+from .permissions import IsJobOwner
+from .filters import JobPostFilter
 from .choices import JobPostStatus
+from .models import JobPost
+from agent.agent_bridge import Agent
 
 # Create your views here.
 
@@ -37,17 +47,36 @@ class JobPostCreateView(generics.CreateAPIView):
             posted_by=self.request.user
         )
 
-# class JobPostListCreateView(generics.ListCreateAPIView):
-#     queryset = JobPost.objects.all()
-#     serializer_class = JobPostSerializer
-#     permission_classes = [IsAuthenticatedOrReadOnly]
+class ResumeAnalysisView(generics.GenericAPIView):
 
-#     filterset_class = JobPostFilter
-#     ordering_fields = ["posted_at", "salary"]
-#     ordering = ["-posted_at"]
+    permission_classes = [IsAuthenticated]
+    serializer_class = ResumeAnalysisSerializer
 
-#     def perform_create(self, serializer):
-#         serializer.save(posted_by=self.request.user)
+    def post(self, request, *args, **kwargs):
+        job_id = self.kwargs.get("job_id")
+        
+        job = get_object_or_404(JobPost, id=job_id)
+        resume_file = request.FILES.get('resume')
+
+        if not resume_file:
+            return Response({"error": "No resume file provided"}, status=status.HTTP_400_BAD_REQUEST)
+
+        resume_content = extract_text_from_pdf(resume_file)
+        if not resume_content:
+            return Response({"error": "Could not extract text from PDF"}, status=status.HTTP_400_BAD_REQUEST)
+
+        job_post_info = get_job_post_info(job)
+
+        ai_service = Agent()
+        analysis = ai_service.analyze_resume_compatibility(
+            resume_content=resume_content,
+            job_post_info=job_post_info
+        )
+
+        if "error" in analysis:
+            return Response(analysis, status=status.HTTP_503_SERVICE_UNAVAILABLE)
+
+        return Response(analysis, status=status.HTTP_200_OK)
 
 class GetOwnerJobPostListView(generics.ListAPIView):
     serializer_class = JobPostSerializer
